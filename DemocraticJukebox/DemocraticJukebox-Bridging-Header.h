c21b5a547b4dacc3188feb05dedf62d644059872
ThahirMaheen <thahirmaheen@icloud.com>
2015-05-29 21:51:35 +0530

    fixed untracked files

---
+++ DemocraticJukebox/DemocraticJukebox-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  DemocraticJukebox-Bridging-Header.h
+//  DemocraticJukebox
+//
+//  Created by Thahir on 29/05/15.
+//  Copyright (c) 2015 Jukebox. All rights reserved.
+//
+
+#ifndef DemocraticJukebox_DemocraticJukebox_Bridging_Header_h
+#define DemocraticJukebox_DemocraticJukebox_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '7.0'
+
+inhibit_all_warnings!
+
+pod 'FBSDKCoreKit',     '~> 4.2'
+pod 'FBSDKLoginKit',    '~> 4.2'
