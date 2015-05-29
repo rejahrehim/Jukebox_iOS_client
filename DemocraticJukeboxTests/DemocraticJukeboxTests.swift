1abb51c9f4f863ad9f9665df6f56e06483d8150b
ThahirMaheen <thahirmaheen@icloud.com>
2015-05-29 20:33:44 +0530

    Initial Commit

---
+++ DemocraticJukeboxTests/DemocraticJukeboxTests.swift
@@ -0,0 +1,36 @@
+//
+//  DemocraticJukeboxTests.swift
+//  DemocraticJukeboxTests
+//
+//  Created by Thahir on 29/05/15.
+//  Copyright (c) 2015 Jukebox. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DemocraticJukeboxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
