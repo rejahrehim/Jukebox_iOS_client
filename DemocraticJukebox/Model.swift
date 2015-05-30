89d4af1b750d9860d5d95176cc0ef4d419a0334b
Thahir Maheen <thahirmaheen@icloud.com>
2015-05-30 10:52:11 +0530

    model

---
+++ DemocraticJukebox/Model.swift
@@ -0,0 +1,37 @@
+//
+//  Model.swift
+//  DemocraticJukebox
+//
+//  Created by Thahir Maheen on 29/05/15.
+//  Copyright (c) 2015 Jukebox. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    
+    /// currently logged in user
+    var currentUser = User()
+    
+    class var sharedModel: Model {
+        struct Singleton {
+            static let sharedModel = Model()
+        }
+        return Singleton.sharedModel
+    }
+    
+    struct Storyboards {
+        static let Main = "Main"
+        static let Login = "Login"
+    }
+    
+    struct Api {
+        static let BaseUrl = "http://192.168.1.147:8081"
+        struct KeyPaths {
+            static let Queue = "/api/v1/queue"
+            static let Songs = "/api/v1/songs"
+            static let History = "/api/v1/history/my"
+            static let Favorites = "/api/v1/favourites"
+        }
+    }
+}
