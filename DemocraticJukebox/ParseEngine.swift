a4b6ba211f9abcc6b6934ffa158752f8d658f0c4
Thahir Maheen <thahirmaheen@icloud.com>
2015-06-04 19:33:33 +0530

    updated parse engine to post data

--- DemocraticJukebox/ParseEngine.swift
+++ DemocraticJukebox/ParseEngine.swift
@@ -10,6 +10,12 @@ import Foundation
 
 class ParseEngine {
     
+    enum HTTPMethod: String {
+        case GET = "GET"
+        case POST = "POST"
+        case DELETE = "DELETE"
+    }
+    
     class var sharedEngine: ParseEngine {
         struct Singleton {
             static let sharedEngine = ParseEngine()
@@ -17,13 +23,19 @@ class ParseEngine {
         return Singleton.sharedEngine
     }
     
-    func fetchData(urlString: String, completionHandler: (data: NSDictionary) -> ()) {
+    func fetchData(keyPath: String, params: [String: String], completionHandler: (data: NSDictionary) -> ()) {
+        
+        // format params into a string to pass into url
+        let paramStrings = params.keys.array.reduce("?") { $0 + "\($1)=\(params[$1]!)&" }
+        
+        // encode the url string
+        let urlString = (Model.Api.BaseUrl + keyPath + paramStrings).stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
         
         // create the url
         let url = NSURL(string: urlString)!
         
         // download data
-        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
             
             // handle errors
             if (error != nil) {
@@ -41,8 +53,59 @@ class ParseEngine {
                     return
                 }
                 
-                // return the result
-                completionHandler(data: jsonData)
+                // return the result in the main queue
+                dispatch_async(dispatch_get_main_queue(), { () in
+                    completionHandler(data: jsonData)
+                })
+            }
+            
+        }.resume()
+    }
+    
+    func postData(keyPath: String, params: [String: String], completionHandler: (data: NSDictionary) -> ()) {
+        
+        // create the url string
+        let urlString = (Model.Api.BaseUrl + keyPath).stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        
+        var dictionaryParams = params
+        dictionaryParams["Authorization"] = Model.sharedModel.currentUser.token
+
+        // create from data from the params dictionary
+        let fromData = NSJSONSerialization.dataWithJSONObject(dictionaryParams, options: NSJSONWritingOptions.PrettyPrinted, error: nil)
+        
+        // configure an NSURLRequest
+        let request = NSMutableURLRequest(URL: NSURL(string: urlString)!)
+        request.HTTPMethod = HTTPMethod.POST.rawValue
+        
+        session.uploadTaskWithRequest(request, fromData: fromData) { (data, response, error) in
+            
+            println("params = \(dictionaryParams)")
+            println("data = \(data)")
+            println("response = \(response)")
+            println("error = \(error)")
+            
+            // handle errors
+            if (error != nil) {
+                println("error: \(error.localizedDescription)")
+                return
+            }
+            
+            // parse data
+            var jsonError: NSError?
+            if let jsonData = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as? NSDictionary {
+                
+                // handle parsing errors
+                if (jsonError != nil) {
+                    println("error: \(jsonError!.localizedDescription)")
+                    return
+                }
+                
+                // return the result in the main queue
+                dispatch_async(dispatch_get_main_queue(), { () in
+                    completionHandler(data: jsonData)
+                })
             }
             
         }.resume()
