deb96d1e0f3561ac1cde9dedb3df3d7124957df7
Thahir Maheen <thahirmaheen@icloud.com>
2015-06-04 19:34:24 +0530

    code cleanup

--- DemocraticJukebox/UIViewControllerExtensions.swift
+++ DemocraticJukebox/UIViewControllerExtensions.swift
@@ -9,8 +9,9 @@
 import Foundation
 
 extension UIViewController {
-    
+
     var appDelegate: AppDelegate {
         return UIApplication.sharedApplication().delegate as! AppDelegate
     }
+    
 }
