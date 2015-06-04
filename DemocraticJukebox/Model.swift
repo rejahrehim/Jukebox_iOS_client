6c70ac1c15d654fe72788a1a4d8ffccd351f84e6
Thahir Maheen <thahirmaheen@icloud.com>
2015-06-04 19:32:45 +0530

    updated Model to add params

--- DemocraticJukebox/Model.swift
+++ DemocraticJukebox/Model.swift
@@ -26,12 +26,16 @@ class Model {
     }
     
     struct Api {
-        static let BaseUrl = "http://192.168.1.147:8081"
+//        static let BaseUrl = "http://192.168.1.147:8081"
+        static let BaseUrl = "http://115.112.144.186:8082"
         struct KeyPaths {
             static let Queue = "/api/v1/queue"
             static let Songs = "/api/v1/songs"
             static let History = "/api/v1/history/my"
             static let Favorites = "/api/v1/favourites"
+        }
+        struct Params {
+            static let Search = "?search"
         }
     }
 }
